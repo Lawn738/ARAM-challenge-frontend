86fb5c7489a5ad481db929cc1eadcbf8b02fb98d
Juuso Kokkonen <juusokokkonen98@gmail.com>
2023-06-01 06:19:34 +0300

    added footer to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from "./Sidebar";
 import ChallengeTest from './Challenge';
+import Footer from './components/footer';
 
 function App() {
   
   return (
     <div className="App">
       {Sidebar()}
+      <Footer />
     </div>
     
   );
